8a3bbb6b8ee21abfd871674b63f82cd38c3b3ed8
Artem <lazarazuy@gmail.com>
2021-09-17 23:38:35 +0200

    add 6th task

---
+++ js/task-06.js
@@ -0,0 +1,33 @@
+const containerInput = document.querySelector('input')
+const focusInput = document.querySelector('#validation-input');
+const lengthInput = containerInput.dataset.length;
+
+
+// focusInput.addEventListener('change', eventing);
+// function eventing(event){
+//     const text=event.currentTarget.value;
+
+//     if (text.length==lengthInput){
+//         focusInput.classList.add('valid');
+//         focusInput.classList.remove('invalid');
+
+//     }
+//     else {
+//         focusInput.classList.add('invalid');
+//         focusInput.classList.remove('valid');
+//     }
+// }
+focusInput.addEventListener('blur',bluring);
+function bluring(event){
+    const text=event.currentTarget.value;
+
+    if (text.length==lengthInput){
+        focusInput.classList.add('valid');
+        focusInput.classList.remove('invalid');
+
+    }
+    else {
+        focusInput.classList.add('invalid');
+        focusInput.classList.remove('valid');
+    }
+}
